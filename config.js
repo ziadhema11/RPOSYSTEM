c5ace974eb7b0a5a2f1f4911d78afd35d0d28773
ziadhema11 <111283628+ziadhema11@users.noreply.github.com>
2022-09-03 23:44:47 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+   
+
+    discord: {
+        token: ['OTkxNzY4OTA1OTg5MjMwNjMz.Gcix32.Zd76QDSIqGYSxDPgZxnWNbSeMnFfsJLEDiWGXQ', '', '', '', '', '']
+                
+              },
+    
+    main_owner: ['910482674915885077','','','',''],
+
+};
